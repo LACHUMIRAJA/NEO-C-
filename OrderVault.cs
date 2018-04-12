1d967af6e9bda33d6054527c63a2959a049d1596
LACHUMIRAJA <36506458+LACHUMIRAJA@users.noreply.github.com>
2018-04-12 18:29:34 +0530

    OrderVault

--- OrderVault.cs
+++ OrderVault.cs
@@ -1,6 +1,8 @@
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
 using System;
+using System.Linq;
+using System.Numerics;
 
 namespace OrderVault
 {
@@ -10,138 +12,155 @@ namespace OrderVault
         public static Object Main(string oper, params Object[] args)
         {
 
-           byte[] vaultOwners=null;
-           byte[] orderHashes = null;
-           uint beginTime=0;
-           uint endTime=0;
-           byte[] approver;
-      
+            
+            //BigInteger orderHashes = 0;
+            BigInteger beginTime;
+            BigInteger endTime;
+            byte[] approver;
+
+           // Storage.Put(Storage.CurrentContext, "OrderHash", orderHashes);
+
             switch (oper)
             {
 
                 case "addOwner":
                     {
-                       
+
                         byte[] newOwner = (byte[])args[0];
+                       
+                        Runtime.Notify(newOwner + " is Added");
+                        Storage.Put(Storage.CurrentContext, newOwner, 1);
+                        Storage.Put(Storage.CurrentContext, newOwner.Concat(Neo.SmartContract.Framework.Helper.AsByteArray("owner")), "owner");
+                        return true;
                         
-                        Runtime.Notify(newOwner+ " is Added");
-                        Storage.Put(Storage.CurrentContext, newOwner, "True");
-                        int i = vaultOwners.Length;
-                        vaultOwners[i] = (newOwner[0]);
-                        Storage.Put(Storage.CurrentContext, vaultOwners, newOwner);
-                        break;
                     }
                 case "removeOwner":
 
                     {
-                        
-                        byte[] owner = (byte[])args[0];
-                        Runtime.Notify(owner+ " is Removed");
-                        Storage.Put(Storage.CurrentContext, owner, "False");
-                       
-                        for (int i = 0; i <= vaultOwners.Length; i++)
-                        {
-                            if (vaultOwners[i] == owner[0])
-                            { 
-                                vaultOwners[i] = 0; 
-                                vaultOwners[i]= vaultOwners[i+1];
-                                
 
-                                return vaultOwners;
-                            }
+                        byte[] reowner = (byte[])args[0];
+                        Runtime.Notify(reowner + " is Removed");
+                        Storage.Put(Storage.CurrentContext, reowner, 0);
+                        if (Storage.Get(Storage.CurrentContext, reowner)[0] == 0)
+                        {
+                            Storage.Delete(Storage.CurrentContext, reowner);
+                            Storage.Delete(Storage.CurrentContext, reowner.Concat(Neo.SmartContract.Framework.Helper.AsByteArray("owner")));
+                            return true;
                         }
-                       
+
                         return false;
                     }
 
                 case "openVault":
                     {
-                        uint startTime = (uint)args[0];
-                        uint closureTime = (uint)args[1];
-
+                       BigInteger startTime = (BigInteger)args[0];
+                       BigInteger closureTime = (BigInteger)args[1];
 
                         byte[] check = Storage.Get(Storage.CurrentContext, "Vault is Open");
                         byte[] check1 = Storage.Get(Storage.CurrentContext, "Vault is Seal");
 
 
-                        uint now = (uint)args[2];// Runtime.Time;
+                       BigInteger now = (BigInteger)args[2];// Runtime.Time;
 
-                        if (check1.Equals("False") && (check.Equals("False") && startTime <= now && closureTime >= now && closureTime >= startTime))
+                        if (check1.Equals(0) && (check.Equals(0) && startTime <= now && closureTime >= now && closureTime >= startTime))
                         {
-                            beginTime = startTime;
-                            endTime = closureTime;
-                           
-                            Storage.Put(Storage.CurrentContext, "Vault is Open", "True");
+                            Storage.Put(Storage.CurrentContext, "starttime", startTime);
+                            Storage.Put(Storage.CurrentContext, "closuretime", closureTime);
+
+                            Storage.Put(Storage.CurrentContext, "Vault is Open", 1);
                             Runtime.Log("Vault is Opened");
-                           
+
                         }
                         else
                         {
-                            Storage.Put(Storage.CurrentContext, "Vault is Open", "False");
-                            Runtime.Log("Vault is not Opened");
+                            Storage.Put(Storage.CurrentContext, "Vault is Open", 0);
+                            if(Storage.Get(Storage.CurrentContext,"Vault is Open")[0]==0)
+                            {
+                                Storage.Delete(Storage.CurrentContext, "Vault is Open");
+                            }
+
+                            Runtime.Log("Vault is Closed");
                         }
-                        return Storage.Get(Storage.CurrentContext,"Vault is Open");
+                        return Storage.Get(Storage.CurrentContext, "Vault is Open");
 
                     }
 
 
                 case "orderVault":
                     {
-                        
-                        approver = (byte[])args[0];
+                        byte[] check = Storage.Get(Storage.CurrentContext, "Vault is Open");
+                        byte[] check1 = Storage.Get(Storage.CurrentContext, "Vault is Seal");
 
-                        vaultOwners = approver;
+                        if (check1.Equals(0) && (check.Equals(0)))
+                            {
 
-                        Storage.Put(Storage.CurrentContext,"Approver" , approver);
-                        Runtime.Log("Approver Setted Successfully");
-                        Storage.Put(Storage.CurrentContext, "vaultOwners", approver);
-                        return true;
+                            approver = (byte[])args[0];
+
+                            Storage.Put(Storage.CurrentContext, "Approver", approver);
+                            Runtime.Log("Approver Setted Successfully");
+                            return true;
+                        }
+                        return false;
                     }
 
                 case "sealVault":
                     {
-                        uint now = (uint)args[0];
-                       
+                        BigInteger now = (BigInteger)args[0];
+
                         byte[] check1 = Storage.Get(Storage.CurrentContext, "Vault is Seal");
 
-                        if (check1.Equals("False"))
+                        if (check1.Equals(0))
+                        {
+                            Storage.Put(Storage.CurrentContext, "closuretime", now);
+
+                            Storage.Put(Storage.CurrentContext, "Vault is Seal", 1);
+
+                            Storage.Put(Storage.CurrentContext, "Vault is Open", 0);
+                            if (Storage.Get(Storage.CurrentContext, "Vault is Open")[0] == 0)
                             {
-                                endTime = now;
-                              
-                                Storage.Put(Storage.CurrentContext, "Vault is Seal", "True");
-                              
-                                Storage.Put(Storage.CurrentContext, "Vault is Open", "False");
-                                Runtime.Log("Vault is closed and Sealed Successfully");
-                           
-                                 return Storage.Get(Storage.CurrentContext, "Vault is Seal");
-                            
+                                Storage.Delete(Storage.CurrentContext, "Vault is Open");
+                            }
+
+                            Runtime.Log("Vault is closed and Sealed Successfully");
+
+                            return Storage.Get(Storage.CurrentContext, "Vault is Seal");
+
                         }
                         Runtime.Log("Vault is not Sealed");
                         return Storage.Get(Storage.CurrentContext, "Vault is Seal");
                     }
 
-               
-                case "getnumoforders":
-                    {
-                        Runtime.Notify(orderHashes.Length);
-                        return orderHashes.Length;
 
-                    }
+                /*case "getnumoforders":
+                    {
+                        BigInteger nooh = Neo.SmartContract.Framework.Helper.AsBigInteger(Storage.Get(Storage.CurrentContext, "OrderHash"));
+                        Runtime.Notify("No. Of OrderHashes " +nooh);
+                        return true;
+
+                    }*/
 
 
             }
 
-            if(oper == "extendVault")
+            if (oper == "extendVault")
             {
-                uint closureTime = (uint)args[0];
-                uint now = (uint)args[1];
+                BigInteger closureTime = (BigInteger)args[0];
+                BigInteger now = (BigInteger)args[1];
+
+               
+
+                beginTime = Neo.SmartContract.Framework.Helper.AsBigInteger(Storage.Get(Storage.CurrentContext, "starttime"));
+
+
 
                 byte[] check = Storage.Get(Storage.CurrentContext, "Vault is Open");
                 byte[] check1 = Storage.Get(Storage.CurrentContext, "Vault is Seal");
-                if (check1.Equals("False") && check.Equals("True") && beginTime <= now && closureTime >= now && closureTime >= beginTime)
+
+                Runtime.Notify("Checking...");
+                if (check1.Equals(0) && check.Equals(1) && beginTime <= now  && closureTime >= now && closureTime >= beginTime)
                 {
-                    endTime = closureTime;
-                    Storage.Put(Storage.CurrentContext, "Vault is Open", "True");
+                    Storage.Put(Storage.CurrentContext, "closuretime", closureTime);
+                    Storage.Put(Storage.CurrentContext, "Vault is Open", 1);
                     Runtime.Log("Vault Time is extended");
                     return true;
                 }
@@ -159,20 +178,23 @@ namespace OrderVault
 
                 byte[] oHash = (byte[])args[0];
                 byte[] orderId = ((byte[])args[1]);
-                uint now = (uint)args[2];
+                BigInteger now = (BigInteger)args[2];
+
+                beginTime = Neo.SmartContract.Framework.Helper.AsBigInteger(Storage.Get(Storage.CurrentContext, "starttime"));
+                endTime = Neo.SmartContract.Framework.Helper.AsBigInteger(Storage.Get(Storage.CurrentContext, "closuretime"));
 
                 byte[] check = Storage.Get(Storage.CurrentContext, "Vault is Open");
                 byte[] check1 = Storage.Get(Storage.CurrentContext, "Vault is Seal");
 
+                //Storage.Put(Storage.CurrentContext, "OrderHash", Neo.SmartContract.Framework.Helper.AsBigInteger(Storage.Get(Storage.CurrentContext, "OrderHash"))+ 1); ;
 
-                if (check1.Equals("False") && check.Equals("True") && (beginTime <= now) && !(endTime >= now) && (endTime >= beginTime))
+                Runtime.Log("Checking....");
+                Runtime.Notify(beginTime, endTime);
+                if (check1.Equals(0) && check.Equals(1) && beginTime <= now && endTime >= now  && endTime >= beginTime)
                 {
-
-
-                    orderHashes = oHash;
-                    // Storage.Put(Storage.CurrentContext, orderHashes, oHash);
-                    Storage.Put(Storage.CurrentContext, orderHashes, "True");
-                    Storage.Put(Storage.CurrentContext, orderId, "True");
+                    Storage.Put(Storage.CurrentContext, oHash.Concat(Neo.SmartContract.Framework.Helper.AsByteArray(" ")),"orderhashes" );
+                    Storage.Put(Storage.CurrentContext, oHash, 1);
+                    Storage.Put(Storage.CurrentContext, orderId,1);
                     Runtime.Log("Well Stored");
                 }
                 else
@@ -186,35 +208,42 @@ namespace OrderVault
 
 
             if (oper == "closeVault")
-             {
+            {
                 byte[] check = Storage.Get(Storage.CurrentContext, "Vault is Open");
                 byte[] check1 = Storage.Get(Storage.CurrentContext, "Vault is Seal");
 
                 uint now = (uint)args[0];
-                if (check1.Equals("False") && check.Equals("True"))
+                if (check1.Equals(0) && check.Equals(1))
                 {
-                    endTime = now;
-                    Storage.Put(Storage.CurrentContext, "Vault is Open", "False");
+                    Storage.Put(Storage.CurrentContext, "closuretime", now);
+                    Storage.Put(Storage.CurrentContext, "Vault is Open", 0);
+
+                    if (Storage.Get(Storage.CurrentContext, "Vault is Open")[0] == 0)
+                    {
+                        Storage.Delete(Storage.CurrentContext, "Vault is Open");
+                    }
+
                     Runtime.Log("Vault is Closed Successfully");
                     return true;
                 }
+
                 Runtime.Log("Vault is does not Closed");
-                return false; 
+                return false;
             }
 
 
 
-            
+
             if (oper == "orderLocated")
             {
                 byte[] hash = (byte[])args[0];
                 byte[] orderId = (byte[])args[1];
 
                 Runtime.Log("Order  Location Checked");
-               // Storage.Get(Storage.CurrentContext, orderHashes);
+                // Storage.Get(Storage.CurrentContext, orderHashes);
                 return Storage.Get(Storage.CurrentContext, orderId);
-               
-                
+
+
 
             }
 
@@ -224,9 +253,8 @@ namespace OrderVault
             return false;
 
 
-            }
+        }
 
-            
-         }
+
+    }
 }
-
